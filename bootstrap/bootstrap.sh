1eb9f85b16bdae7c9488b1092db7e9ac9b9783a3
Gogs <gogitservice@gmail.com>
2014-11-16 14:11:33 +0100

    init

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,4 +4,4 @@ apt-get autoremove
 apt-get -f install -y
 apt-get upgrade -y
 apt-get install mc curl git ssh python2.7 python-requests  -y
-curl http://git.aydo.com/aydo/docs_0disk/raw/master/getstarted/bootstrap.py | python 
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/bootstrap/bootstrap.py | python 
--- bootstrap/readme.md
+++ bootstrap/readme.md
@@ -1,5 +1,4 @@
 
 do bootstrap do
-´´´
-curl http://git.aydo.com/aydo/docs_0disk/raw/master/getstarted/bootstrap.sh | bash
-´´´
+
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/bootstrap/bootstrap.sh | bash
