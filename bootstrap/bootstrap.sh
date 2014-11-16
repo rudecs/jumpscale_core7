1e03fbb0daa1f85f87f1accc9d12feb7ac3a92f1
Gogs <gogitservice@gmail.com>
2014-11-16 14:27:59 +0100

    init

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,2 +1,5 @@
 apt-get install mc curl git ssh python2.7 python-requests  -y
-curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/bootstrap/bootstrap.py | python 
+set -ex
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/bootstrap/bootstrap.py > /tmp/bootstrap.py
+cd /tmp
+python bootstrap.py
