05dbf713287db8b8cc1b14acbe713e1937175ee2
Gogs <gogitservice@gmail.com>
2014-11-19 08:30:26 +0100

    init

--- install/web/bootstrap34.py
+++ install/web/bootstrap34.py
@@ -1,12 +1,5 @@
 #BOOTSTRAP CODE
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib import urlopen
-import random
-handle = urlopen("https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/InstallTools.py?%s"%random.randint(1, 10000000)) #this is to protect against caching proxy servers
-exec(handle.read())
-
+import InstallTools
 
 #look at methods in https://github.com/Jumpscale/jumpscale_core7/blob/master/install/InstallTools.py to see what can be used
 #there are some easy methods to allow git manipulation, copy of files, execution of items 
