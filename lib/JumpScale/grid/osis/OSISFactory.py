a858521d8b05e79f05ea724ea36ecfe331c56c88
Sherif Zain <zains@codescalers.com>
2014-12-02 15:18:49 +0200

    Fixed: get osis client hrd

--- lib/JumpScale/grid/osis/OSISFactory.py
+++ lib/JumpScale/grid/osis/OSISFactory.py
@@ -178,7 +178,7 @@ class OSISFactory:
         # osisjp=j.packages.findNewest(name="osis_client",domain="jumpscale")
         # osisjp.load(instance=instance)
         # if osisjp.isInstalled():
-        hrdinstance = j.core.hrd.get('/opt/jumpscale7/hrd/jumpscale/osis_client/%s' % instance)
+        hrdinstance = j.core.hrd.get('/opt/jumpscale7/hrd/jumpscale/osis_client/%s.hrd' % instance)
         if hrdinstance:
             ipaddr=hrdinstance.get("osis.client.addr")
             port=int(hrdinstance.get("osis.client.port"))
