370d3c8d36c7b3e0cb12d870006afe186d1a4f18
despiegk <kristof@incubaid.com>
2014-12-20 16:00:55 +0200

    init

---
+++ install/install.sh
@@ -0,0 +1,5 @@
+apt-get install mc curl git ssh python2.7 python-requests  -y
+set -ex
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
+cd /tmp
+python /tmp/bootstrap_web.py
