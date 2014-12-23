3e3b7a2688015636af6d7d383c58b9cb2c06e7c0
Reem Khamis <khamisr@codescalers.com>
2014-12-23 11:54:22 +0200

    Adding base libraries to JumpScale7

---
+++ lib/JumpScale/base/__init__.py
@@ -0,0 +1,15 @@
+from JumpScale import j
+
+class Empty():
+	pass
+
+j.base=Empty()
+
+from . import FSWALKER
+from . import FS
+from . import REGEXTOOL
+from . import TIMER
+from . import BYTEPROCESSOR
+from . import ERRORHANDLER
+from . import LOADER
+
