20f0087f21f2ddd3695c290ee64d73774a0eed6a
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-01-20 17:59:45 +0400

    dont reinstall ssh, we are installing from remote so ssh is already there

--- install/install_python_web.sh
+++ install/install_python_web.sh
@@ -1,4 +1,4 @@
-apt-get install mc curl git ssh python2.7 python-requests  -y
+apt-get install mc curl git python2.7 python-requests  -y
 set -ex
 curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/web/bootstrap_web.py > /tmp/bootstrap_web.py
 cd /tmp
