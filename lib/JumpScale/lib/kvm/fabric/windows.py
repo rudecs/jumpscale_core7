1d9ed68092257de1436b08a7995155700966a38e
Sherif Zain <zains@codescalers.com>
2015-01-21 14:48:58 +0200

    Fixed: windows fab module

--- lib/JumpScale/lib/kvm/fabric/windows.py
+++ lib/JumpScale/lib/kvm/fabric/windows.py
@@ -1,9 +1,15 @@
-from fabric.api import task, run
+from fabric.api import task, run, settings
 
 @task
 def setupNetwork(ifaces):
-    for iface, config in ifaces.iteritems():
-        if iface == 'eth1':
-            run('netsh interface ip set address name="%s" static %s %s gateway=%s' % (iface, config[0], config[1], config[2]))
-        else:
-            run('netsh interface ip set address name="%s" static %s %s' % (iface, config[0], config[1]))
+    with settings(user='Administrator'):
+        for iface, config in ifaces.iteritems():
+            if iface == 'eth1':
+                run('netsh interface ip set address name="%s" static %s %s gateway=%s' % (iface, config[0], config[1], config[2]))
+            else:
+                run('netsh interface ip set address name="%s" static %s %s' % (iface, config[0], config[1]))
+
+@task
+def pushSshKey(sshkey):
+    with settings(user='Administrator'):
+        run('touch /etc/authorized_keys; echo "%s" > /etc/authorized_keys' % sshkey)
