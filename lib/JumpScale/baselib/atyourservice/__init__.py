71db8bfd7383dcd51b05d74ab67d7a992c6c5b50
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-03-30 16:31:37 +0400

    update __init__.py

--- lib/JumpScale/baselib/atyourservice/__init__.py
+++ lib/JumpScale/baselib/atyourservice/__init__.py
@@ -1,6 +1,7 @@
 from JumpScale import j
-from .AtYourServiceFactory import AtYourServiceFactory
-from .AtYourServiceRemoteFactory import AtYourServiceRemoteFactory
 
-j.atyourservice = AtYourServiceFactory()
-j.atyourservice.remote = AtYourServiceRemoteFactory()
+def cb():
+    from .AtYourServiceFactory import AtYourServiceFactory
+    return AtYourServiceFactory()
+
+j._register('atyourservice', cb)
