4526469fdd3dc0c0c49ebeb22f50f8d1fc5313da
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-04-06 10:46:32 +0400

    full install with @ys branch

--- install/install_updateubuntu.sh
+++ install/install_updateubuntu.sh
@@ -3,4 +3,4 @@ apt-get autoremove -y
 apt-get -f install -y
 apt-get upgrade -y
 apt-get install mc curl git ssh python2.7 python-requests  -y
-curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/web/bootstrap.py | python 
+curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/%40ys/install/web/bootstrap.py | python 
--- install/web/install_develenv.py
+++ install/web/install_develenv.py
@@ -2,7 +2,7 @@
 #BOOTSTRAP CODE
 from urllib import urlopen
 import random
-handle = urlopen("https://raw.githubusercontent.com/Jumpscale/jumpscale_core/master/install/InstallTools.py?%s"%random.randint(1, 10000000))
+handle = urlopen("https://raw.githubusercontent.com/Jumpscale/jumpscale_core/%40ys/install/InstallTools.py?%s"%random.randint(1, 10000000))
 exec(handle.read())
 
 print do.prepareUbuntu14Development(js=True)
