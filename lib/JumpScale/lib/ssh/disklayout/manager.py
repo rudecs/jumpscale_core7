0ed9ba2caa06fc49914f83409f2717445322c919
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-05-19 09:22:34 +0200

    More abstraction

--- lib/JumpScale/lib/ssh/disklayout/manager.py
+++ lib/JumpScale/lib/ssh/disklayout/manager.py
@@ -1,6 +1,7 @@
 from JumpScale import j
 from . import lsblk
 from . import mount
+from . import disks
 
 
 class DiskManagerFactory(object):
@@ -20,12 +21,47 @@ class DiskManager(object):
         if self.con.file_exists(hrdpath):
             return j.core.hrd.get(content=self.con.file_read(hrdpath))
 
-    def getinfo(self):
-        output = self.con.run(lsblk.COMMAND)
-        disks = lsblk.parse(self.con, output)
+    def _loaddisks(self, blks):
+        """
+        Parses the output of command
+        `lsblk -abnP -o NAME,TYPE,UUID,FSTYPE,SIZE`
 
+        Output must look like that
+        NAME="sda" TYPE="disk" UUID="" FSTYPE="" SIZE="256060514304"
+        NAME="sda1" TYPE="part" UUID="1db378f5-4e49-4fb7-8000-051fe77b23ea"
+            FSTYPE="btrfs" SIZE="256059465728"
+        NAME="sr0" TYPE="rom" UUID="" FSTYPE="" SIZE="1073741312"
+        """
+        devices = []
+        disk = None
+        for blk in blks:
+            name = blk['NAME']
+            if blk['TYPE'] == 'disk':
+                disk = disks.DiskInfo(self.con, name, blk['SIZE'])
+                devices.append(disk)
+            elif blk['TYPE'] == 'part':
+                if disk is None:
+                    raise Exception(
+                        ('Parition "%s" does not have a parent disk' %
+                            blk['NAME'])
+                    )
+                part = disks.PartitionInfo(
+                    self.con, name, blk['SIZE'],
+                    blk['UUID'], blk['FSTYPE'],
+                    blk['MOUNTPOINT']
+                )
+                disk.partitions.append(part)
+            else:
+                # don't care about outher types.
+                disk = None
+
+        return devices
+
+    def getDisks(self):
+        blks = lsblk.lsblk(self.con)
+        devices = self._loaddisks(blks)
         # loading hrds
-        for disk in disks:
+        for disk in devices:
             for partition in disk.partitions:
                 if partition.fstype == 'swap':
                     continue
@@ -39,4 +75,4 @@ class DiskManager(object):
                         hrd = self._loadhrd(mnt.path)
 
                 partition.hrd = hrd
-        return disks
+        return devices
