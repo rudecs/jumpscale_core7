fae158822950838a352d64c7086e653f037d3239
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-05-21 11:32:06 +0200

    Implement erase all static hosts

--- lib/JumpScale/lib/ssh/openwrt/dhcp.py
+++ lib/JumpScale/lib/ssh/openwrt/dhcp.py
@@ -11,14 +11,23 @@ class Interface(object):
 
     @property
     def section(self):
+        """
+        The low level UCI section
+        """
         return self._section
 
     @property
     def name(self):
+        """
+        Interface name
+        """
         return self._section['interface']
 
     @property
     def start(self):
+        """
+        Lowest leased address as offset from the network address
+        """
         return self._section.get('start')
 
     @start.setter
@@ -27,6 +36,9 @@ class Interface(object):
 
     @property
     def limit(self):
+        """
+        Maximum number of leased addresses.
+        """
         return self._section.get('limit')
 
     @limit.setter
@@ -35,6 +47,9 @@ class Interface(object):
 
     @property
     def leasetime(self):
+        """
+        Expiry time of leased addresses, minimum is 2 minutes (2m).
+        """
         return self._section.get('leasetime')
 
     @leasetime.setter
@@ -43,6 +58,9 @@ class Interface(object):
 
     @property
     def enabled(self):
+        """
+        Enable DHCP on this interface
+        """
         return not bool(self._section.get('ignore'))
 
     @enabled.setter
@@ -62,10 +80,16 @@ class Host(object):
 
     @property
     def section(self):
+        """
+        The low level UCI section for that host
+        """
         return self._section
 
     @property
     def name(self):
+        """
+        Host name
+        """
         return self._section['name']
 
     @name.setter
@@ -74,6 +98,9 @@ class Host(object):
 
     @property
     def mac(self):
+        """
+        Host MAC
+        """
         return self._section['mac']
 
     @mac.setter
@@ -82,6 +109,9 @@ class Host(object):
 
     @property
     def ip(self):
+        """
+        Host IP
+        """
         return self._section['ip']
 
     @ip.setter
@@ -108,6 +138,9 @@ class DHCP(object):
 
     @property
     def package(self):
+        """
+        The low level UCI interface
+        """
         if self._package is None:
             self._package = self._wrt.get(DHCP.PACKAGE)
         return self._package
@@ -122,9 +155,19 @@ class DHCP(object):
 
     @property
     def hosts(self):
+        """
+        Return all static hosts
+        """
         return map(Host, self.package.find('host'))
 
     def addHost(self, name, mac, ip):
+        """
+        Add static host
+
+        :name: host name
+        :mac: host mac
+        :ip: host IP
+        """
         sec = self.package.add('host')
         sec['name'] = name
         sec['mac'] = mac
@@ -133,12 +176,27 @@ class DHCP(object):
         return Host(sec)
 
     def removeHost(self, name):
+        """
+        Remove static host
+
+        :name: host name
+        """
         for host in self.hosts:
             if host.name == name:
                 self.package.remove(host.section)
                 return
 
+    def erase(self):
+        """
+        Remove all static hosts
+        """
+        for sec in self.package.find('host'):
+            self.package.remove(sec)
+
     def commit(self):
+        """
+        Commit changes to openwrt and restart DHCP
+        """
         self._wrt.commit(self.package)
 
         con = self._wrt.connection
