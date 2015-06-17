b203257e2394efa9814973e5e32e8a9c21863eb6
hamdy <hamdy.farag@codescalers.com>
2015-06-17 15:55:44 +0200

    oauth add support for logout URL

--- lib/JumpScale/baselib/oauth/OauthFactory.py
+++ lib/JumpScale/baselib/oauth/OauthFactory.py
@@ -14,8 +14,8 @@ class OauthFactory(object):
     def __init__(self):
         j.logger.consolelogCategories.append('oauth')
 
-    def get(self, addr='', accesstokenaddr='', id='', secret='', scope='', redirect_url='', user_info_url='', instance='github'):        
-        return OauthInstance(addr, accesstokenaddr, id, secret, scope, redirect_url, user_info_url, instance)
+    def get(self, addr='', accesstokenaddr='', id='', secret='', scope='', redirect_url='', user_info_url='', logout_url='', instance='github'):        
+        return OauthInstance(addr, accesstokenaddr, id, secret, scope, redirect_url, user_info_url, logout_url, instance)
 
     def log(self,msg,category='',level=5):
         category = 'oauth.%s'%category
