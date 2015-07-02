e89edbc6fb550ade82a330834286c7ae2465d6d5
hamdy <hamdy.farag@codescalers.com>
2015-07-02 10:35:17 +0200

    Add support for swagger into ROS

--- apps/ros/app/runner.py
+++ apps/ros/app/runner.py
@@ -3,21 +3,17 @@ from JumpScale.baselib import cmdutils
 
 import os
 import sys
-
+import swagger
 from eve import Eve
 from eve.utils import config
 from eve.render import send_response
 from eve_docs import eve_docs
 from eve_docs.config import get_cfg
-
-from flask.ext.bootstrap import Bootstrap
-
 from eve_sqlalchemy import SQL
 from eve_sqlalchemy.validation import ValidatorSQL
 
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
-
 from . import modelloader
 from . import api
 from .converters import osis2mongo
@@ -28,6 +24,7 @@ import importlib
 
 import copy
 
+STATIC_PATH = '/opt/jumpscale7/jslib/swagger/'
 
 HOOKSMAP = {'on_insert_%s': 'pre_create',
             'on_inserted_%s': 'post_create',
@@ -49,6 +46,7 @@ config.ITEM_LOOKUP_FIELD = ID_FIELD
 BASE_SETTINGS = {
         'DEBUG': True,
         'ID_FIELD': ID_FIELD,
+        'IF_MATCH':False,
         'ITEM_URL': 'regex("[a-f0-9\-]+")',
         'ITEM_LOOKUP_FIELD': ITEM_LOOKUP_FIELD,
         'RESOURCE_METHODS': ['GET', 'POST'],
@@ -61,20 +59,6 @@ BASE_SETTINGS = {
 BASE_MONGO_SETTINGS = BASE_SETTINGS.copy()
 BASE_MONGO_SETTINGS['MONGO_HOST'] = '127.0.0.1'
 BASE_MONGO_SETTINGS['MONGO_PORT'] = 27017
-
-def expose_docs(app):
-    Bootstrap(app)
-    @app.route('/docs/spec.json')
-    def specs():
-        cfg = copy.deepcopy(get_cfg())
-        for model in cfg['domains'].values():
-            for path in model.values():
-                for method in path.values():
-                    for param in method['params']:
-                        if 'default' in param and callable(param['default']):
-                            param['default'] = None
-        return send_response(None, [cfg])
-    app.register_blueprint(eve_docs, url_prefix='/docs')
 
 def hookevents(namespace, app):
     for model in app.settings['DOMAIN'].keys():
@@ -97,8 +81,8 @@ def prepare_mongoapp(namespace, models):
     my_settings['MONGO_QUERY_BLACKLIST'] = []
     my_settings['DOMAIN'] = osis2mongo.generateDomain(models)
     # init application
-    app = Eve('osis', settings=my_settings)
-    expose_docs(app)
+    app = Eve('osis', settings=my_settings, static_url_path=STATIC_PATH)
+    swagger.expose_docs(app, STATIC_PATH)
     hookevents(namespace, app)
     return app
 
@@ -113,19 +97,23 @@ def prepare_sqlapp(namespace, models, sqluri):
     my_settings['IF_MATCH'] = False
     my_settings['SQLALCHEMY_RECORD_QUERIES'] = True
     my_settings['DOMAIN'] = osis2sqlalchemy.generateDomain(namespace, models)
-    app = Eve(validator=ValidatorSQL, data=SQL, settings=my_settings)
+    app = Eve(validator=ValidatorSQL, data=SQL, settings=my_settings, static_url_path=STATIC_PATH)
     db = app.data.driver
     common.Base.metadata.bind = db.engine
     db.Model = common.Base
     db.create_all()
-    expose_docs(app)
+    swagger.expose_docs(app, STATIC_PATH)
     hookevents(namespace, app)
     return app
 
 def start(basedir, hrd):
     port = hrd.getInt('instance.param.port')
     mongdb_instance = hrd.get('instance.param.mongodb.connection', '')
+    autoreload = True if hrd.get('instance.param.autoreload', '').lower() == "yes" else False
     sqluri = hrd.get('instance.param.sqlalchemy.uri', '')
+    use_reloader = True if hrd.get('instance.param.eve.use_reloader', '').lower() == 'yes' else False
+    use_debugger = True if hrd.get('instance.param.eve.use_debugger', '').lower() == 'yes' else False
+
     if mongdb_instance:
         mongohrd = j.application.getAppInstanceHRD('mongodb_client', mongdb_instance) 
         BASE_SETTINGS['MONGO_HOST'] = mongohrd.get('instance.param.addr')
@@ -148,5 +136,5 @@ def start(basedir, hrd):
         apps['/api'] = apiapp
         application = DispatcherMiddleware(apiapp, apps)
         # let's roll
-        run_simple('0.0.0.0', port, application, use_reloader=True, use_debugger=True)
+        run_simple('0.0.0.0', port, application, use_reloader=use_reloader, use_debugger=use_debugger)
 
