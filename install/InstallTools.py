95a883538f9954bcd7337a7a8dd8f662831ae7d3
kristof de spiegeleer <kristof@incubaid.com>
2015-07-22 11:34:51 +0200

    ?

--- install/InstallTools.py
+++ install/InstallTools.py
@@ -83,6 +83,11 @@ class InstallTools():
             # print ("OUR OWN EXCEPTHOOK")
             sys.excepthook = self.excepthook
 
+        from IPython import embed
+        print "DEBUG NOW sdsdsd"
+        embed()
+        
+
     def excepthook(self, ttype, pythonExceptionObject, tb):
 
         if isinstance(pythonExceptionObject, HaltException):
--- install/ssh/InstallTools.py
+++
@@ -1,1 +0,0 @@
-/opt/code/github/jumpscale/jumpscale_core7/install/InstallTools.py
