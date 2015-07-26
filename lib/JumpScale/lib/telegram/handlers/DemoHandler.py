2dc0cdecc0a6f490080abeb784ad1adc2ed67404
Jo De Boeck <deboeck.jo@gmail.com>
2015-07-26 12:59:39 +0200

    Telegram: Add sticker example

--- lib/JumpScale/lib/telegram/handlers/DemoHandler.py
+++ lib/JumpScale/lib/telegram/handlers/DemoHandler.py
@@ -4,6 +4,7 @@ import json
 from JumpScale import j
 
 AUDIOFILE = "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3"
+STICKERFILE = "https://telegram-stickers.github.io/public/stickers/flags/8.png"
 
 class DemoHandler:
 
@@ -11,8 +12,10 @@ class DemoHandler:
         url="http://www.greenitglobe.com/gig/.files/img/logo.png"
         self.image = "%s/giglogo.png"%j.dirs.tmpDir
         self.sound = "%s/test.mp3"%j.dirs.tmpDir
+        self.sticker = "%s/sticker.png"%j.dirs.tmpDir
         j.do.download(url,to=self.image,overwrite=False)
         j.do.download(AUDIOFILE,to=self.sound,overwrite=False)
+        j.do.download(STICKERFILE,to=self.sticker,overwrite=False)
         self.once = True
 
     def on_text(self, tg, message):
@@ -31,7 +34,7 @@ class DemoHandler:
         if message.text=="send photo":
             print tg.send_photo(message.chat.id, self.image,reply_to_message_id="", reply_markup="")
         if message.text=="send sticker":
-            result = tg.send_sticker(message.chat.id, self.image,reply_to_message_id="", reply_markup="")
+            result = tg.send_sticker(message.chat.id, self.sticker,reply_to_message_id="", reply_markup="")
             print result
         if message.text=="send sound":
             print tg.send_audio(message.chat.id, self.sound,reply_to_message_id="", reply_markup="")
