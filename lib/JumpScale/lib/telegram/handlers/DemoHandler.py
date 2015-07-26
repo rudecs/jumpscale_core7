3e66ccedcf99e61bed05a9b8275cf407ef3abc54
Jo De Boeck <deboeck.jo@gmail.com>
2015-07-26 12:52:53 +0200

    Telegram: cleanup example add sticker + sound

--- lib/JumpScale/lib/telegram/handlers/DemoHandler.py
+++ lib/JumpScale/lib/telegram/handlers/DemoHandler.py
@@ -3,42 +3,40 @@ from datetime import datetime
 import json
 from JumpScale import j
 
+AUDIOFILE = "https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3"
+
 class DemoHandler:
 
     def __init__(self):
-
         url="http://www.greenitglobe.com/gig/.files/img/logo.png"
-        j.do.download(url,to="%s/giglogo.png"%j.dirs.tmpDir,overwrite=False)
-
-
-        self.once=True
+        self.image = "%s/giglogo.png"%j.dirs.tmpDir
+        self.sound = "%s/test.mp3"%j.dirs.tmpDir
+        j.do.download(url,to=self.image,overwrite=False)
+        j.do.download(AUDIOFILE,to=self.sound,overwrite=False)
+        self.once = True
 
     def on_text(self, tg, message):
-            
-        # markup={}
-        # markup["force_reply"]=True
-
-        # tg.send_message(message.chat.id, "this is me",reply_to_message_id=None,reply_markup=json.dumps(markup))               
-
-        if self.once==True:
-            tg.send_message(message.chat.id, "Welcome to the demo bot")
+        if self.once:
+            print tg.send_message(message.chat.id, "Welcome to the demo bot")
             print "send init photo"
-            tg.send_photo(message.chat.id, "%s/giglogo.png"%j.dirs.tmpDir,reply_to_message_id="", reply_markup="")
+            print tg.send_photo(message.chat.id, self.image,reply_to_message_id="", reply_markup="")
             print "photo send"
             self.once=False
 
-
         markup={}
-        markup["keyboard"]=[["yes"],["no"],["1","2","3"],["stop"],["send photo"]]
+        markup["keyboard"]=[["yes"],["no"],["stop"],["send photo", "send sticker", "send sound"]]
         markup["resize_keyboard"]=True
         markup["one_time_keyboard"]=True
 
         if message.text=="send photo":
-            tg.send_photo(message.chat.id, "%s/giglogo.png"%j.dirs.tmpDir,reply_to_message_id="", reply_markup="")
-
-        if not message.text=="stop":
-
-            tg.send_message(message.chat.id, "Please fill in",reply_to_message_id=None,reply_markup=json.dumps(markup))                    
+            print tg.send_photo(message.chat.id, self.image,reply_to_message_id="", reply_markup="")
+        if message.text=="send sticker":
+            result = tg.send_sticker(message.chat.id, self.image,reply_to_message_id="", reply_markup="")
+            print result
+        if message.text=="send sound":
+            print tg.send_audio(message.chat.id, self.sound,reply_to_message_id="", reply_markup="")
+        elif not message.text=="stop":
+            print tg.send_message(message.chat.id, "Please fill in",reply_to_message_id=None,reply_markup=json.dumps(markup))
 
 
         
