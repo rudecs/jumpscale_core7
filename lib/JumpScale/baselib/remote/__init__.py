4bd39634e385c394dd225abce4bbd59f4e81a35d
rkhamis <khamisr@codescalers.com>
2015-08-03 11:00:28 +0200

    Remove import of an obselete lib

--- lib/JumpScale/baselib/remote/__init__.py
+++ lib/JumpScale/baselib/remote/__init__.py
@@ -3,6 +3,5 @@ import JumpScale.baselib.remote.fabric
 import JumpScale.baselib.remote.avahi
 import JumpScale.baselib.remote.ssh
 import JumpScale.baselib.remote.remotesystem
-import JumpScale.baselib.remote.cluster
 import JumpScale.baselib.expect
 
