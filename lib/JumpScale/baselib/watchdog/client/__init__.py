0384bdc9fbac387510e74426273b8cfc000eda32
rkhamis <khamisr@codescalers.com>
2015-08-18 15:47:52 +0200

    Removal of obselete code

--- lib/JumpScale/baselib/watchdog/client/__init__.py
+++
@@ -1,7 +0,0 @@
-from JumpScale import j
-
-from .WatchdogClient import *
-
-j.base.loader.makeAvailable(j, 'tools.watchdog')
-
-j.tools.watchdog.client=WatchdogClient()
--- lib/JumpScale/baselib/watchdog/manager/__init__.py
+++
@@ -1,7 +0,0 @@
-from JumpScale import j
-
-from .WatchdogFactory import *
-
-j.base.loader.makeAvailable(j, 'tools.watchdog')
-
-j.tools.watchdog.manager=WatchdogFactory()
