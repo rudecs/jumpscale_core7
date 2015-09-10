7baa6f4f6f344daf93293d5d4adbb31773a9a112
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-09-10 12:37:48 +0200

    PEP8 formatting

--- lib/JumpScale/baselib/git/GitClient.py
+++ lib/JumpScale/baselib/git/GitClient.py
@@ -1,43 +1,45 @@
 from JumpScale import j
 
+
 class GitClient(object):
 
-    def __init__(self, baseDir):
+    def __init__(self, base_dir):
+        if not j.system.fs.exists(path=base_dir):
+            j.events.inputerror_critical("git repo on %s not found." % base_dir)
 
-        if not j.system.fs.exists(path=baseDir):
-            j.events.inputerror_critical("git repo on %s not found."%baseDir)
+        # split path to find parts
+        base_dir = base_dir.replace("\\", "/")
+        if base_dir.find("/code/") == -1:
+            j.events.inputerror_critical(
+                "jumpscale code management always requires path in form of $somewhere/code/$type/$account/$reponame")
+        base = base_dir.split("/code/", 1)[1]
 
-        #split path to find parts
-        baseDir=baseDir.replace("\\","/")
-        if baseDir.find("/code/")==-1:
-            j.events.inputerror_critical("jumpscale code management always requires path in form of $somewhere/code/$type/$account/$reponame")
-        base=baseDir.split("/code/",1)[1]
+        if base.count("/") != 2:
+            j.events.inputerror_critical(
+                "jumpscale code management always requires path in form of $somewhere/code/$type/$account/$reponame")
 
-        if base.count("/")!=2:
-            j.events.inputerror_critical("jumpscale code management always requires path in form of $somewhere/code/$type/$account/$reponame")
+        self.type, self.account, self.name = base.split("/")
 
-        self.type,self.account,self.name=base.split("/")
+        self.base_dir = base_dir
 
-        self.baseDir = baseDir
-
-        gitconfig="%s/.git/config"%baseDir
-        config=j.system.fs.fileGetContents(gitconfig)
+        gitconfig = "%s/.git/config" % base_dir
+        config = j.system.fs.fileGetContents(gitconfig)
 
         self.remoteUrl = None
-        self.branchName = None
+        self.branch_name = None
 
         for line in config.split("\n"):
-            line=line.strip()
-            if line=="":
+            line = line.strip()
+            if line == "":
                 continue
-            if line.find("url =")!=-1 or line.find("url=")!=-1 :
-                self.remoteUrl=line.split("=")[1].strip()
+            if line.find("url =") != -1 or line.find("url=") != -1:
+                self.remoteUrl = line.split("=")[1].strip()
 
             if line.startswith("[branch"):
-                self.branchName=line.split("\"")[1].strip()
-        
-        if self.remoteUrl==None or self.branchName==None:
-            j.events.inputerror_critical("git repo on %s is corrupt could not find branch & remote url"%baseDir)
+                self.branch_name = line.split("\"")[1].strip()
+
+        if self.remoteUrl is None or self.branch_name is None:
+            j.events.inputerror_critical("git repo on %s is corrupt could not find branch & remote url" % base_dir)
 
         self._repo = None
 
@@ -53,58 +55,58 @@ class GitClient(object):
         import git
         if not self._repo:
             j.system.process.execute("git config --global http.sslVerify false")
-            if not j.system.fs.exists(self.baseDir):
+            if not j.system.fs.exists(self.base_dir):
                 self._clone()
             else:
-                self._repo = git.Repo(self.baseDir)
+                self._repo = git.Repo(self.base_dir)
         return self._repo
 
     def init(self):
         self.repo
 
-    def switchBranch(self, branchName):
-        self.repo.git.checkout(branchName)
+    def switchBranch(self, branch_name):
+        self.repo.git.checkout(branch_name)
 
     def getModifiedFiles(self):
-        result={}
-        result["D"]=[]
-        result["N"]=[]
-        result["M"]=[]
-        result["R"]=[]
+        result = {}
+        result["D"] = []
+        result["N"] = []
+        result["M"] = []
+        result["R"] = []
 
-        cmd="cd %s;git status --porcelain"%self.baseDir
-        rc,out=j.system.process.execute(cmd)
+        cmd = "cd %s;git status --porcelain" % self.base_dir
+        rc, out = j.system.process.execute(cmd)
         for item in out.split("\n"):
-            if item.strip()=="":
+            if item.strip() == "":
                 continue
-            item2=item.split(" ",1)[1]
+            item2 = item.split(" ", 1)[1]
             result["N"].append(item2)
-        
+
         for diff in self.repo.index.diff(None):
-            path=diff.a_blob.path
+            path = diff.a_blob.path
             if diff.deleted_file:
                 result["D"].append(path)
             elif diff.new_file:
                 result["N"].append(path)
             elif diff.renamed:
-                result["R"].append(path)            
-            else:                
+                result["R"].append(path)
+            else:
                 result["M"].append(path)
         return result
 
     def addRemoveFiles(self):
-        cmd='cd %s;git add -A :/'%self.baseDir
+        cmd = 'cd %s;git add -A :/' % self.base_dir
         j.system.process.execute(cmd)
         # result=self.getModifiedFiles()
         # self.removeFiles(result["D"])
         # self.addFiles(result["N"])
 
     def addFiles(self, files=[]):
-        if files!=[]:
+        if files != []:
             self.repo.index.add(files)
 
     def removeFiles(self, files=[]):
-        if files!=[]:
+        if files != []:
             self.repo.index.remove(files)
 
     def pull(self):
@@ -113,12 +115,12 @@ class GitClient(object):
     def fetch(self):
         self.repo.git.fetch()
 
-    def commit(self, message='',addremove=True):
+    def commit(self, message='', addremove=True):
         if addremove:
             self.addRemoveFiles()
         self.repo.index.commit(message)
 
-    def push(self,force=False):
+    def push(self, force=False):
         if force:
             self.repo.git.push('-f')
         else:
@@ -174,19 +176,19 @@ coverage.xml
 # Sphinx documentation
 docs/_build/
 '''
-        ignorefilepath = j.system.fs.joinPaths(self.baseDir, '.gitignore')
+        ignorefilepath = j.system.fs.joinPaths(self.base_dir, '.gitignore')
         if not j.system.fs.exists(ignorefilepath):
             j.system.fs.writeFile(ignorefilepath, gitignore)
         else:
             lines = gitignore.split('\n')
             inn = j.system.fs.fileGetContents(ignorefilepath)
-            linesExisting = inn.split('\n')
+            lines = inn.split('\n')
             linesout = []
-            for line in linesExisting:
+            for line in lines:
                 if line.strip():
                     linesout.append(line)
             for line in lines:
-                if line not in linesExisting and line.strip():
+                if line not in lines and line.strip():
                     linesout.append(line)
             out = '\n'.join(linesout)
             if out.strip() != inn.strip():
--- lib/JumpScale/baselib/git/GitFactory.py
+++ lib/JumpScale/baselib/git/GitFactory.py
@@ -2,53 +2,53 @@ from .GitClient import GitClient
 from JumpScale import j
 import os
 
+
 class GitFactory:
     def __init__(self):
         j.logger.consolelogCategories.append("git")
 
-
     def get(self, basedir):
         """
-        PLEASE USE SSH, see http://despiegk.gitbooks.io/jumpscale/content/Howto/how_to_use_git.html for more details
+        PLEASE USE SSH, see http://gig.gitbooks.io/jumpscale/content/Howto/how_to_use_git.html for more details
         """
         return GitClient(basedir)
 
-    def log(self,msg,category="",level=5):
-        category="git.%s"%category
-        category=category.rstrip(".")
-        j.logger.log(msg,category=category,level=level)
+    def log(self, msg, category="", level=5):
+        category = "git.%s" % category
+        category = category.rstrip(".")
+        j.logger.log(msg, category=category, level=level)
 
-    def find(self,account=None,name=None,interactive=False,returnGitClient=False):
+    def find(self, account=None, name=None, interactive=False, returnGitClient=False): # NOQA
         """
         walk over repo's known on system
         2 locations are checked
             ~/code
             /opt/code
         """
-        if name==None:
-            name=""
-        if account==None:
-            account=""
-                
-        accounts=[]
-        accounttofind=account
+        if name is None:
+            name = ""
+        if account is None:
+            account = ""
+
+        accounts = []
+        accounttofind = account
 
         def checkaccount(account):
             # print accounts
             # print "%s %s"%(account,accounttofind)
             if account not in accounts:
-                if accounttofind.find("*")!=-1:
-                    if accounttofind=="*" or account.startswith(accounttofind.replace("*","")):
+                if accounttofind.find("*") != -1:
+                    if accounttofind == "*" or account.startswith(accounttofind.replace("*", "")):
                         accounts.append(account)
-                elif accounttofind!="":
-                    if account.lower().strip()==accounttofind.lower().strip():
+                elif accounttofind != "":
+                    if account.lower().strip() == accounttofind.lower().strip():
                         accounts.append(account)
                 else:
                     accounts.append(account)
             # print accountsunt in accounts
             return account in accounts
 
-        def _getRepos(codeDir,account=None,name=None):
+        def _getRepos(codeDir, account=None, name=None): # NOQA
             """
             @param interactive if interactive then will ask to select repo's out of the list
             @para returnGitClient if True will return gitclients as result
@@ -57,7 +57,7 @@ class GitFactory:
             [[type,account,reponame,path]]
 
             the type today is git or github today
-            all std git repo's go to git 
+            all std git repo's go to git
 
             ```
             #example
@@ -66,52 +66,55 @@ class GitFactory:
             ```
 
             """
-            result=[]
-            repos=[]
-            for top in j.system.fs.listDirsInDir(codeDir, recursive=False, dirNameOnly=True, findDirectorySymlinks=True):
-                for account in j.system.fs.listDirsInDir("%s/%s"%(j.dirs.codeDir,top), recursive=False, dirNameOnly=True, findDirectorySymlinks=True):
+            repos = []
+            for top in j.system.fs.listDirsInDir(codeDir, recursive=False,
+                                                 dirNameOnly=True, findDirectorySymlinks=True):
+                for account in j.system.fs.listDirsInDir("%s/%s" % (j.dirs.codeDir, top), recursive=False,
+                                                         dirNameOnly=True, findDirectorySymlinks=True):
                     if checkaccount(account):
-                        accountdir="%s/%s/%s"%(j.dirs.codeDir,top,account)
-                        if j.system.fs.exists(path="%s/.git"%accountdir):
-                            raise RuntimeError("there should be no .git at %s level"%accountdir)
+                        accountdir = "%s/%s/%s" % (j.dirs.codeDir, top, account)
+                        if j.system.fs.exists(path="%s/.git" % accountdir):
+                            raise RuntimeError("there should be no .git at %s level" % accountdir)
                         else:
-                            for reponame in j.system.fs.listDirsInDir("%s/%s/%s"%(j.dirs.codeDir,top,account), recursive=False, dirNameOnly=True, findDirectorySymlinks=True):
-                                repodir="%s/%s/%s/%s"%(j.dirs.codeDir,top,account,reponame)
-                                if j.system.fs.exists(path="%s/.git"%repodir):
-                                    if name.find("*")!=-1:
-                                        if name=="*" or reponame.startswith(name.replace("*","")):
-                                            repos.append([top,account,reponame,repodir])
-                                    elif name!="":
-                                        if reponame.lower().strip()==name.lower().strip():
-                                            repos.append([top,account,reponame,repodir])
+                            for reponame in j.system.fs.listDirsInDir("%s/%s/%s" % (j.dirs.codeDir, top, account),
+                                                                      recursive=False, dirNameOnly=True,
+                                                                      findDirectorySymlinks=True):
+                                repodir = "%s/%s/%s/%s" % (j.dirs.codeDir, top, account, reponame)
+                                if j.system.fs.exists(path="%s/.git" % repodir):
+                                    if name.find("*") != -1:
+                                        if name == "*" or reponame.startswith(name.replace("*", "")):
+                                            repos.append([top, account, reponame, repodir])
+                                    elif name != "":
+                                        if reponame.lower().strip() == name.lower().strip():
+                                            repos.append([top, account, reponame, repodir])
                                     else:
-                                        repos.append([top,account,reponame,repodir])
+                                        repos.append([top, account, reponame, repodir])
             return repos
 
-        j.system.fs.createDir(j.system.fs.joinPaths(os.getenv("HOME"),"code"))
-        repos=_getRepos(j.dirs.codeDir,account,name)
-        repos+=_getRepos(j.system.fs.joinPaths(os.getenv("HOME"),"code"),account,name)
+        j.system.fs.createDir(j.system.fs.joinPaths(os.getenv("HOME"), "code"))
+        repos = _getRepos(j.dirs.codeDir, account, name)
+        repos += _getRepos(j.system.fs.joinPaths(os.getenv("HOME"), "code"), account, name)
 
         accounts.sort()
-        
+
         if interactive:
-            result=[]
-            if len(repos)>20:
-                print ("Select account to choose from, too many choices.")
-                accounts=j.console.askChoiceMultiple(accounts)
-                
-            repos=[item for item in repos if item[1] in accounts]
+            result = []
+            if len(repos) > 20:
+                print("Select account to choose from, too many choices.")
+                accounts = j.console.askChoiceMultiple(accounts)
 
-            #only ask if * in name or name not specified
-            if name.find("*")==-1 or name==None:
-                repos=j.console.askArrayRow(repos)
+            repos = [item for item in repos if item[1] in accounts]
 
-        result=[]
+            # only ask if * in name or name not specified
+            if name.find("*") == -1 or name is None:
+                repos = j.console.askArrayRow(repos)
+
+        result = []
         if returnGitClient:
-            for top,account,reponame,repodir in repos:
-                cl=self.get(repodir)
+            for top, account, reponame, repodir in repos:
+                cl = self.get(repodir)
                 result.append(cl)
         else:
-            result=repos
-        
+            result = repos
+
         return result
