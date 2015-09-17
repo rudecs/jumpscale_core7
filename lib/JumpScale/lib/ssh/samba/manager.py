d694b319c79314357678232393e18ba7b08146b2
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-09-17 12:42:12 +0400

    samba SAL: create /etc/samba/smb.conf if doesn't exist yet

--- lib/JumpScale/lib/ssh/samba/manager.py
+++ lib/JumpScale/lib/ssh/samba/manager.py
@@ -13,37 +13,37 @@ class SMBUser(object):
     def __init__(self, verbose=False):
         # self._smb = cmd_sambatool(self._stdout, self._stderr)
         self._verbose = verbose
-    
+
     def _smbrun(self, args):
         output = j.system.process.run("samba-tool user " + args, False, True, 10, False)
-        
+
         # if stdout is empty, we return the first line of stderr
         if output[1] == '':
             lines = output[2].split('\n')
             lines = [lines[0], 0]
         else:
             lines = output[1].split('\n')
-        
+
         lines.pop()
         return lines
-    
+
     def _format(self, output):
         if output[0].startswith("ERROR("):
             if self._verbose:
                 print output[0]
-            
+
             return False
-        
+
         return True
-    
+
     def list(self):
         output = self._smbrun("list")
         return output
-        
+
     def remove(self, username):
         output = self._smbrun("delete " + username)
         return self._format(output)
-    
+
     def add(self, username, password):
         output = self._smbrun("add " + username + " " + password)
         j.system.process.run("bash /etc/samba/update-uid.sh", True, False)
@@ -55,57 +55,60 @@ class SMBShare(object):
         self._load()
 
     def _load(self):
+        if not j.system.fs.exists(path=CONFIG_FILE):
+            j.system.fs.createDir(j.system.fs.getParent(CONFIG_FILE))
+            j.system.fs.createEmptyFile(CONFIG_FILE)
         data = StringIO('\n'.join(line.strip() for line in open(CONFIG_FILE)))
         self._config.readfp(data)
-    
+
     def get(self, sharename):
         # special share which we don't handle
         if sharename in EXCEPT_SHARES:
             return False
-            
+
         # share name not found
         if not self._config.has_section(sharename):
             return False
-        
+
         return self._config.items(sharename)
-        
+
     def remove(self, sharename):
         # don't touch special shares (global, ...)
         if sharename in EXCEPT_SHARES:
             return False
-        
+
         if not self._config.has_section(sharename):
             return False
-        
+
         self._config.remove_section(sharename)
         return True
-    
+
     def add(self, sharename, path, options={}):
         # share already exists or is denied
         if self._config.has_section(sharename):
             return False
-        
+
         if sharename in EXCEPT_SHARES:
             return False
-        
+
         # set default options
         self._config.add_section(sharename)
         self._config.set(sharename, 'path', path)
-        
+
         # set user defined options
         for option in options:
             self._config.set(sharename, option, options[option])
-        
+
         return True
-        
+
     def commit(self):
         with open(CONFIG_FILE, 'wb') as configfile:
             self._config.write(configfile)
-        
+
         # reload config
         j.system.process.killProcessByName('smbd', signal.SIGHUP)
         j.system.process.killProcessByName('nmbd', signal.SIGHUP)
-            
+
         return True
 
 class Samba:
@@ -113,25 +116,25 @@ class Samba:
         self._users = SMBUser(True)
         self._shares = SMBShare()
         self._con = con;
-    
+
     def getShare(self, sharename):
         return self._shares.get(sharename)
-        
+
     def removeShare(self, sharename):
         return self._shares.remove(sharename)
-        
+
     def addShare(self, sharename, path, options={}):
         return self._shares.add(sharename, path, options)
-        
+
     def commitShare(self):
         return self._shares.commit()
-    
+
     def listUsers(self):
         return self._users.list()
-    
+
     def removeUser(self, username):
         return self._users.remove(username)
-    
+
     def addUser(self, username, password):
         return self._users.add(username, password)
 
