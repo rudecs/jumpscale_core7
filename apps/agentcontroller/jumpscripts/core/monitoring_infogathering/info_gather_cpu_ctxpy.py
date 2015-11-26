4e6b71d9800c3f0879b85d889c198c1c0cb377d9
Jo De Boeck <deboeck.jo@gmail.com>
2015-11-26 23:02:35 +0200

    Fix script that calculate ctx switch

--- apps/agentcontroller/jumpscripts/core/monitoring_infogathering/info_gather_cpu_ctxpy.py
+++ apps/agentcontroller/jumpscripts/core/monitoring_infogathering/info_gather_cpu_ctxpy.py
@@ -27,46 +27,46 @@ def action():
     nid = j.application.whoAmI.nid
 
     try:
-       results = osis.search('system', 'stats', 
-            'select derivative(value,10s) from "%s_%s_cpu.num_ctx_switches.gauge" where time > now() - 1h ' % 
+       oldest = osis.search('system', 'stats', 
+            'select value from "%s_%s_cpu.num_ctx_switches.gauge" where time > now() - 1h limit 1' % 
+            (gid, nid))
+
+       newest = osis.search('system', 'stats', 
+            'select value from "%s_%s_cpu.num_ctx_switches.gauge" where time > now() - 1h order by time desc limit 1' % 
             (gid, nid))
     except RemoteException , e:
         return [{'category':'CPU', 'state':'ERROR', 'message':'influxdb halted cannot access data'}]
          
     res=list()
-    for noderesult in results['raw'].get('series', []):
-        parts = noderesult['name'].split('.')[0]
-        avgctx = abs(noderesult['values'][0][-1] / 3600.) #thresholds or per second
-        type = parts.split('_')[2]
-        diff = list()
-        value = 0 
-        count = 0
-        for  entry in noderesult['values']:
-            if entry[1]:
-                value += entry[1]
-                count += 1
-        avgctx = value/count
-        level = None
-        result = dict()
-        result ['state'] = 'OK'
-        result ['message'] = 'CPU contextswitch value is: %s per s' % avgctx
-        result ['category'] = 'CPU'
-        if avgctx > 100000:
-            level = 1
-            result ['state'] = 'ERROR'
 
-        elif avgctx > 600000:
-            level = 2
-            result ['state'] = 'WARNING'
+    try:
+        newvalue = newest['raw']['series'][0]['values'][0][1]
+        oldestvalue = oldest['raw']['series'][0]['values'][0][1]
+    except (KeyError,IndexError):
+        return [{'category':'CPU', 'state':'WARNING', 'message':'Not enough data collected'}]
 
-        if level:
-            msg = 'CPU contextswitch is to high current value: %s per s' % avgctx
-            eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=level, type='OPERATIONS')
-            eco.nid = nid
-            eco.gid = gid
-            eco.process()
+    avgctx = (newvalue - oldestvalue) / 3600.
+    level = None
+    result = dict()
+    result['state'] = 'OK'
+    result['message'] = 'CPU contextswitch value is: %.2f/s' % avgctx
+    result['category'] = 'CPU'
+    if avgctx > 100000:
+        level = 1
+        result ['state'] = 'ERROR'
 
-        res.append(result)
+    elif avgctx > 600000:
+        level = 2
+        result ['state'] = 'WARNING'
+
+    if level:
+        msg = 'CPU contextswitch is to high current value: %.2f/s' % avgctx
+        eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=level, type='OPERATIONS')
+        eco.nid = nid
+        eco.gid = gid
+        eco.process()
+    
+    res.append(result)
     return res
 
 
