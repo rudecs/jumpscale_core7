c0f0cf6e9a14624fe6e089d36f4a72607b2236e1
Jo De Boeck <deboeck.jo@gmail.com>
2016-09-01 12:53:12 +0200

    Update QOS

--- lib/JumpScale/lib/netqos/qos.py
+++ lib/JumpScale/lib/netqos/qos.py
@@ -4,12 +4,12 @@ from JumpScale import j
 class QOS(object):
     def limitNic(self, name, rate, burst, prio=100):
         self.removeLimit(name)
-        cmd = '''\
-        # Add 2 rules, one to narrow down bw utilisation in any case and one to still allow interactivity for ssh to the vm (we could envision rdp as a third rule)
+        cmd '''\
         # first create ingress policer
         tc qdisc add dev {iface} handle ffff: ingress
+
         # allow ssh to continue to work
-        tc filter add dev eno1 parent ffff: protocol ip \
+        tc filter add dev {iface} parent ffff: protocol ip \
             pref {prio} u32 match ip dport 22 0xffff flowid 1: \
             police rate 100mbit burst 100mbit \
             mpu 0 action continue continue
@@ -18,9 +18,11 @@ class QOS(object):
             pref {prio} u32 match ip dport 3389 0xffff flowid 1: \
             police rate 100mbit burst 100mbit \
             mpu 0 action continue continue
+
         # police everything else FROM the VM (really tight, most protocols will stop working)
         tc filter add dev {iface} parent ffff:  u32 \
-            match u32 0 0 police rate {rate} burst {burst}
+            match u32 0 0 police rate {rate} burst {burst} \
+            flowid 1: action drop continue
         '''.format(iface=name, prio=prio, burst=burst, rate=rate)
         j.system.process.execute(cmd)
 
