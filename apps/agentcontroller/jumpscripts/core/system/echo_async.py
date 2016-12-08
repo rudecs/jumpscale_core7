f3932d5e93ceddf6bfab850f9289e53c74784e62
Jo De Boeck <deboeck.jo@gmail.com>
2016-12-08 22:33:19 +0200

    Set asynctest on correct queue

--- apps/agentcontroller/jumpscripts/core/system/echo_async.py
+++ apps/agentcontroller/jumpscripts/core/system/echo_async.py
@@ -10,6 +10,7 @@ author = "kristof@incubaid.com"
 license = "bsd"
 version = "1.0"
 category = "monitor.healthcheck"
+queue = 'process'
 async = True
 roles = []
 period = 600
@@ -18,6 +19,7 @@ log = True
 
 def action(msg=[{'category': 'JSAgent', 'message': 'Async test', 'state': 'OK'}]):
     return msg
+
 
 if __name__ == "__main__":
     print(action("It works"))
