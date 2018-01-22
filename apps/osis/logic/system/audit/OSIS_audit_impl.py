46cb374b842fc24ea63b089eeeaa1ad9154dbb52
Jo De Boeck <deboeck.jo@gmail.com>
2018-01-22 11:28:43 +0200

    Keep audits for 30days (#54)
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- apps/osis/logic/system/audit/OSIS_audit_impl.py
+++ apps/osis/logic/system/audit/OSIS_audit_impl.py
@@ -2,4 +2,4 @@ from JumpScale import j
 from JumpScale.grid.osis.OSISStoreMongo import OSISStoreMongo
 
 class mainclass(OSISStoreMongo):
-    TTL = 3600 * 24 * 5 # 5 days
+    TTL = 3600 * 24 * 30 # 30 days
