fa5176e9fa5dbda8a310e8ceb1492757b3432b89
Ahmed El-Sayed <ahmed.m.elsayed93@gmail.com>
2018-07-29 09:28:06 +0200

    handle multiple itsyouonlne instances

--- lib/JumpScale/baselib/oauth/OauthFactory.py
+++ lib/JumpScale/baselib/oauth/OauthFactory.py
@@ -15,7 +15,7 @@ class OauthFactory(object):
         j.logger.consolelogCategories.append('oauth')
 
     def get(self, addr='', accesstokenaddr='', id='', secret='', scope='', redirect_url='', user_info_url='', logout_url='', instance='github'):
-        if instance == 'itsyouonline':
+        if instance.startswith('itsyouonline'):
             return ItsYouOnline(addr, accesstokenaddr, id, secret, scope, redirect_url, user_info_url, logout_url, instance)
         else:
             return OauthInstance(addr, accesstokenaddr, id, secret, scope, redirect_url, user_info_url, logout_url, instance)
