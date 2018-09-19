aa74e0ce285340ae93e2f89e590eee00061cd939
alichaddad <chaddada@greenitglobe.com>
2018-09-19 16:42:57 +0200

    update dumper code

--- lib/JumpScale/lib/aggregator/Dumper.py
+++ lib/JumpScale/lib/aggregator/Dumper.py
@@ -3,19 +3,23 @@ from NetworkScanner import NetworkScanner
 import multiprocessing
 import time
 import logging
+from Queue import Empty
 
 
 NUM_WORKERS = 4
 
 
-def _process(dumper, ip, port, queue):
-    print('processing %s:%s' % (ip, port))
+
+def _process(dumper, ip, port, queue, errorqueue):
+    logging.info('processing %s:%s' % (ip, port))
     try:
         redis = j.clients.redis.getRedisClient(ip, port)
         now = int(time.time())
         dumper.dump(redis)
+        queue.put_nowait((ip, port))
     except Exception:
-        print("Failed to process redis '%s:%s'" % (ip, port))
+        logging.exception("Failed to process redis '%s:%s'" % (ip, port))
+        errorqueue.put_nowait((ip, port))
     finally:
         # workers must have some rest (1 sec) before moving to next
         # ip to process
@@ -23,37 +27,54 @@ def _process(dumper, ip, port, queue):
             # process took very short time. Give worker time to rest
             time.sleep(1)
 
-        queue.put_nowait((ip, port))
-
 class BaseDumper(object):
-    def __init__(self, cidr, ports=[6379]):
+    def __init__(self, cidr, ports=[6379], scaninterval=300):
         logging.root.setLevel(logging.INFO)
 
         self._cidr = cidr
-        scanner = NetworkScanner(cidr, ports)
-        self._candidates = scanner.scan()
+        self._scaninterval = scaninterval
+        self._scanner = NetworkScanner(cidr, ports)
+        self._circulation = set()
+
+
+    def update_queue(self, queue):
+        candidates = self._scanner.scan()
+        for ip, ports in candidates.items():
+            for port in ports:
+                item = (ip, port)
+                if item not in self._circulation:
+                    self._circulation.add(item)
+                    queue.put_nowait((ip, port))
 
     def start(self, workers=NUM_WORKERS):
         manager = multiprocessing.Manager()
         queue = manager.Queue()
-        for ip, ports in self.candidates.items():
-            for port in ports:
-                queue.put_nowait((ip, port))
+        errorqueue = manager.Queue()
+        self.update_queue(queue)
 
         pool = multiprocessing.Pool(workers)
+        scantime = time.time()
 
         while True:
-            ip, port = queue.get()
-            pool.apply_async(_process, (self, ip, port, queue))
+            if scantime + self._scaninterval < time.time():
+                self.update_queue(queue)
+                scantime = time.time()
+
+            while not errorqueue.empty():
+                erroritem = errorqueue.get()
+                if erroritem in self._circulation:
+                    self._circulation.remove(erroritem)
+            try:
+                ip, port = queue.get(timeout=5)
+                pool.apply_async(_process, (self, ip, port, queue, errorqueue))
+            except Empty:
+                # queue is empty carry on
+                pass
 
     @property
     def cidr(self):
         return self._cidr
 
-    @property
-    def candidates(self):
-        return self._candidates
-        
     def dump(self, redis):
         """
         Dump, gets a redis connection. It must process the queues of redis until there is no more items to
